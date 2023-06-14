79025380e7c0a275becb5c1f18124737f0751ce1
manujtrehan <trehan.manuj@gmail.com>
2023-06-13 22:17:34 -0400

    First pass on IK - need to test

--- challenge2/inc/StewartIK.h
+++ challenge2/inc/StewartIK.h
@@ -11,37 +11,43 @@ using namespace Eigen;
 class StewartIK
 {
 private:
-    // base and platform radii
+    // base and platform radii (m)
     float m_baseRadius;
     float m_platformRadius;
-    // half of the anchor angles
+    // half of the anchor angles (rad)
     float m_baseAngle;
     float m_platformAngle;
 
     // Actuator limits
-    // actuator length with 0 extension
+    // actuator length with 0 extension (m)
     float m_minLegLength;
-    // actuator length with max extension
+    // actuator length with max extension (m)
     float m_maxLegLength;
 
-    // joint angles in order for the base and platform anchors
+    // joint angles in order for the base and platform anchors (rad)
     VectorXf m_baseAnchorAngles;
     VectorXf m_platformAnchorAngles;
-    // vector from the base to the platform at the home position
+    // vector from the base to the platform at the home position (0, 0, z) (m)
     VectorXf m_basePlatformVec;
-    // current leg lengths
+    // current leg lengths (m)
     VectorXf m_currLegLengths;
 
-    // x, y, z anchor coordinates for the base and platform anchors
+    // x, y, z anchor coordinates for the base and platform anchors wrt their origins (m)
     MatrixXf m_baseAnchorCoords;
     MatrixXf m_platformAnchorCoords;
 
 public:
     /*
-    * Constructor
-    * @brief Initializes the StewartIK class with the required angles, lengths and anchor coordinates
-    * also sets the minLegLength based on the input base platform distance
-    */
+     * Constructor
+     * @brief Initializes the StewartIK class with the required angles, lengths and anchor coordinates
+     * also sets the minLegLength based on the input base platform distance
+     * @param bR base radius (m)
+     * @param pR platform radius (m)
+     * @param bA half of the base anchor angle (rad)
+     * @param pA half of the platform anchor angle (rad)
+     * @param basePlatformDist distance (z) between the base and platform at the home position (m)
+     * @param maxExtend maximum extension of the actuators (m)
+     */
     StewartIK(
         float bR,
         float pR,
@@ -49,6 +55,26 @@ public:
         float pA,
         float basePlatformDist,
         float maxExtend);
+
+    /*
+     * IK solver
+     * @brief Calculates acutator lengths to reach the input 6 DOF pose - l = T + H + R.P - B
+     * @param x y z - desired platform translation in platform frame wrt platform home position (m)
+     * @param roll pitch yaw - rotation of platform frame wrt base frame (rad)
+     */
+    void updateLengthsIK(
+        float x,
+        float y,
+        float z,
+        float roll,
+        float pitch,
+        float yaw);
+
+    /*
+     * Set default actuator lengths
+     * @brief Sets the minimum and maximum actuator lengths - joint limits
+     */
+    void setMinMaxLength(float maxExtend);
 };
 
 #endif
--- challenge2/src/StewartIK.cpp
+++ challenge2/src/StewartIK.cpp
@@ -11,6 +11,7 @@ StewartIK::StewartIK(
     float c1 = EIGEN_PI / 3;
     float c2 = 2 * c1;
 
+    // set the default base to platform vector (only z separation)
     m_basePlatformVec.resize(3);
     m_basePlatformVec << 0, 0, basePlatformDist;
 
@@ -23,7 +24,7 @@ StewartIK::StewartIK(
                             (-c2 - m_baseAngle),
                             (-c2 + m_baseAngle);
 
-    // platform anchor angles - anticlockwise - in order
+    // platform anchor angles - anticlockwise - in order (platform rotated pi/3 anticlockwise wrt base in home position)
     m_platformAnchorAngles.resize(NUM_JOINTS);
     m_platformAnchorAngles << c1 + (-c2 + m_platformAngle),
                                 c1 + -m_platformAngle,
@@ -56,12 +57,47 @@ StewartIK::StewartIK(
     m_platformAnchorCoords *= m_platformRadius;
     m_platformAnchorCoords.transposeInPlace(); // (6 x 3) -> (3 x 6)
 
+    // init inverse kinematics
     MatrixXf initLegVecs = (m_platformAnchorCoords - m_baseAnchorCoords).colwise() + m_basePlatformVec;
     m_currLegLengths = initLegVecs.colwise().norm();
+
     std::cout << m_currLegLengths << std::endl;
+    updateLengthsIK(0, 0, 0, 0, 0, 0);
+    std::cout << "After:" << std::endl << m_currLegLengths << std::endl;
+
+    // set the max valid leg length based on the extension
+    setMinMaxLength(maxExtend);
 }
 
-int main()
+void StewartIK::setMinMaxLength(float maxExtend)
+{
+    m_minLegLength = m_currLegLengths(0);
+    m_maxLegLength = m_minLegLength + maxExtend;
+}
+
+void StewartIK::updateLengthsIK(
+    float x,
+    float y,
+    float z,
+    float rollAngle,
+    float pitchAngle,
+    float yawAngle)
+{
+    VectorXf trans(3);
+    trans << x, y, z;
+
+    AngleAxisf roll(rollAngle, Vector3f::UnitZ());
+    AngleAxisf yaw(yawAngle, Vector3f::UnitY());
+    AngleAxisf pitch(pitchAngle, Vector3f::UnitX());
+    Quaternionf quat = roll * yaw * pitch;
+
+    // l = T + H + R.P - B
+    MatrixXf r_p_b = (quat.toRotationMatrix() * m_platformAnchorCoords) - m_baseAnchorCoords;
+    MatrixXf legVecs = r_p_b.colwise() + (trans + m_basePlatformVec);
+    m_currLegLengths = legVecs.colwise().norm();
+}
+
+    int main()
 {
     StewartIK ik = StewartIK(0.5, 0.2, 0.11, 0.11, 0.2, 0.3);
     return 0;
